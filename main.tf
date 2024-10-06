6886365da95d4291a0984f338c35fdb65a117479
Priya259-ping <priya1420241@gmail.com>
2024-10-06 15:35:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "web" {
   ami           = "ami-0fff1b9a61dec8a5f"
   instance_type = "t2.micro"
-  key_name = "priya"
+  key_name = "Priya"
 vpc_security_group_ids = ["sg-0199005f141190f84"]
   tags = {
     Name = "hari-web"
