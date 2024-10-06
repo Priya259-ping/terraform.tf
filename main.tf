269f56e19ede4d8229af1e4cf7198972fe47339a
Priya259-ping <priya1420241@gmail.com>
2024-10-06 15:34:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0fff1b9a61dec8a5f"
+  instance_type = "t2.micro"
+  key_name = "priya"
+vpc_security_group_ids = ["sg-0199005f141190f84"]
+  tags = {
+    Name = "hari-web"
+  }
+  }
